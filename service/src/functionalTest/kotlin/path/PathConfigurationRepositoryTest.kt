dc1e604c34e6babd9f3afd93049d6808934e774c
Daniel Aiken <dmaiken13@gmail.com>
2025-08-05 17:41:54 -0600

    Rename a class

--- service/src/functionalTest/kotlin/path/PathConfigurationRepositoryTest.kt
+++ service/src/functionalTest/kotlin/path/PathConfigurationRepositoryTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class PathConfigurationServiceTest {
+class PathConfigurationRepositoryTest {
     @Test
     fun `fetch returns a path configuration when the path matches exactly`() =
         testInMemory(
