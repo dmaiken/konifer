96325b181ae7d693dc17e744da30dd25b9f51bdc
Daniel Aiken <dmaiken13@gmail.com>
2025-11-08 18:13:16 -0700

    import

--- service/src/main/kotlin/io/Koin.kt
+++ service/src/main/kotlin/io/Koin.kt
@@ -10,6 +10,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.path.pathModule
 import io.r2dbc.spi.ConnectionFactory
+import io.s3.s3Module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
