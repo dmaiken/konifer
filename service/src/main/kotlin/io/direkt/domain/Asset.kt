e62007c33b1398418366b59a895bdee6cddbd7ce
Daniel Aiken <dmaiken13@gmail.com>
2025-12-02 21:03:29 -0700

    Start work on Asset domain

---
+++ service/src/main/kotlin/io/direkt/domain/Asset.kt
@@ -0,0 +1,110 @@
+package io.direkt.domain
+
+import io.direkt.asset.handler.AssetSource
+import io.direkt.asset.model.StoreAssetRequest
+import java.time.LocalDateTime
+import java.util.UUID
+
+@JvmInline value class AssetId(val value: UUID)
+
+sealed interface Asset {
+    val id: AssetId
+    val path: String
+    val entryId: Long?
+    val alt: String?
+    val labels: Map<String, String>
+    val tags: Set<String>
+    val source: AssetSource
+    val sourceUrl: String?
+    val createdAt: LocalDateTime
+    val modifiedAt: LocalDateTime
+
+    class New private constructor(
+        override val id: AssetId,
+        override val path: String,
+        override val entryId: Long?,
+        override val alt: String?,
+        override val labels: Map<String, String>,
+        override val tags: Set<String>,
+        override val source: AssetSource,
+        override val sourceUrl: String?,
+        override val createdAt: LocalDateTime,
+        override val modifiedAt: LocalDateTime
+    ): Asset {
+        companion object {
+            /**
+             * Looks like some assistive devices truncate alts after 125 characters
+             */
+            private const val MAX_ALT_LENGTH: Int = 125
+
+            /**
+             * Inspired from AWS limit
+             */
+            private const val MAX_LABEL_KEY_LENGTH: Int = 128
+
+            /**
+             * Inspired from AWS limit
+             */
+            private const val MAX_LABEL_VALUE_LENGTH: Int = 256
+
+            /**
+             * Inspired from AWS limit
+             */
+            private const val MAX_LABELS: Int = 50
+
+            private const val MAX_TAG_VALUE_LENGTH: Int = 256
+
+            fun fromHttpRequest(path: String, request: StoreAssetRequest): New {
+                val now = LocalDateTime.now()
+                return New(
+                    id = AssetId(UUID.randomUUID()),
+                    path = path,
+                    entryId = null,
+                    alt = request.alt,
+                    labels = request.labels,
+                    tags = request.tags,
+                    source = request.url?.let {
+                        AssetSource.URL
+                    } ?: AssetSource.UPLOAD,
+                    sourceUrl = request.url,
+                    createdAt = now,
+                    modifiedAt = now,
+                )
+            }
+        }
+
+        init {
+            validate()
+        }
+
+        private fun validate() {
+            if (alt != null && alt.length > MAX_ALT_LENGTH) {
+                throw IllegalArgumentException("Alt exceeds max length of $MAX_ALT_LENGTH")
+            }
+            if (labels.any { it.key.length > MAX_LABEL_KEY_LENGTH || it.value.length > MAX_LABEL_VALUE_LENGTH }) {
+                throw IllegalArgumentException("Labels exceed max length of ($MAX_LABEL_KEY_LENGTH, $MAX_LABEL_VALUE_LENGTH)")
+            }
+            if (labels.size > MAX_LABELS) {
+                throw IllegalArgumentException("Cannot have more than $MAX_LABELS labels")
+            }
+            if (tags.any { it.length > MAX_TAG_VALUE_LENGTH }) {
+                throw IllegalArgumentException("Tags exceed max length of $MAX_TAG_VALUE_LENGTH")
+            }
+        }
+    }
+
+    class Pending(
+        override val id: AssetId,
+        override val path: String,
+        override val entryId: Long?,
+        override val alt: String?,
+        override val labels: Map<String, String>,
+        override val tags: Set<String>,
+        override val source: AssetSource,
+        override val sourceUrl: String?,
+        override val createdAt: LocalDateTime,
+        override val modifiedAt: LocalDateTime
+    ): Asset {
+
+    }
+}
