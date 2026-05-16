10b627c238c8a04f016ccb24392cd551be15b89e
Daniel Aiken <47787825+dmaiken@users.noreply.github.com>
2026-05-16 14:42:03 -0600

    Support Update API in client (#109)

---
+++ client/src/commonTest/kotlin/io/konifer/client/update/KoniferClientUpdateTest.kt
@@ -0,0 +1,41 @@
+package io.konifer.client.update
+
+import io.konifer.client.KoniferClient
+import io.konifer.client.KoniferResponse
+import io.konifer.client.harness.httpClient
+import io.konifer.client.metadata.createMetadataResponse
+import io.konifer.common.http.StoreAssetRequest
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class KoniferClientUpdateTest :
+    FunSpec({
+        test("should be able to update an asset") {
+            val request =
+                StoreAssetRequest(
+                    tags = setOf("tag"),
+                    labels = mapOf("test" to "test"),
+                    alt = "alt",
+                )
+            val expectedResponse = createMetadataResponse()
+            val httpClient =
+                httpClient {
+                    configureMockEngineHappy(
+                        expectedPath = "/assets/users/123/-/entry/2",
+                        request = request,
+                        response = expectedResponse,
+                    )
+                }
+
+            val koniferClient = KoniferClient(httpClient)
+
+            val actualResponse =
+                koniferClient.updateAsset(
+                    path = "/users/123",
+                    request = request,
+                    entryId = 2,
+                )
+            actualResponse::class shouldBe KoniferResponse.Success::class
+            (actualResponse as KoniferResponse.Success<*>).body shouldBe expectedResponse
+        }
+    })
